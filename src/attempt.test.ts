7778f433ff3bcbe691c2eb5c761e5d0b230cad6f
Simon Fraser <simon-fraser@users.noreply.github.com>
2025-04-15 14:33:24 +0000

    test: satisfied linting for test file

--- src/attempt.test.ts
+++ src/attempt.test.ts
@@ -4,7 +4,7 @@ import { attempt } from './attempt'
 
 describe('attempt', () => {
   test('should return the result of the operation', async () => {
-    const operation = async () => 42
+    const operation = async (): Promise<number> => 42
     const [error, result] = await attempt(operation)
 
     expect(error).toBeNull()
@@ -12,7 +12,7 @@ describe('attempt', () => {
   })
 
   test('should return an error if the operation throws', async () => {
-    const operation = async () => {
+    const operation = async (): Promise<Error> => {
       throw new Error('Test error')
     }
     const [error, result] = await attempt(operation)
@@ -22,7 +22,7 @@ describe('attempt', () => {
   })
 
   test('should return an error if the operation returns a rejected promise', async () => {
-    const operation = async () => await Promise.reject(new Error('Test error'))
+    const operation = async (): Promise<never> => await Promise.reject(new Error('Test error'))
     const [error, result] = await attempt(operation)
 
     expect(error).toBeInstanceOf(Error)
